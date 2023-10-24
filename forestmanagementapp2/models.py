6555cf81a75be8a4f492109a891330079943b168
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-10-24 22:14:21 +0200

    Ok, it's working, I guess. See you tomorrow

--- forestmanagementapp2/models.py
+++ forestmanagementapp2/models.py
@@ -2,19 +2,27 @@ from django.db import models
 
 
 class FORET(models.Model):
-    nom_foret = models.TextField(unique=True, primary_key=True)
+    nom_foret = models.TextField(primary_key=True)
     localisation = models.TextField()
     densite = models.IntegerField()
     superfice = models.IntegerField()
     quantite_eau = models.IntegerField()
     composition_sol = models.TextField()
 
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
+
 
 class ORGANISM(models.Model):
-    nom_organisme = models.TextField(unique=True, primary_key=True)
+    nom_organisme = models.TextField(primary_key=True)
     nutrition = models.TextField()
     facteur_emission = models.IntegerField()
     quantite_consomméé_CO2 = models.IntegerField()
+
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
 
 
 class GARDE(models.Model):
@@ -28,11 +36,19 @@ class GARDE(models.Model):
     mail_garde = models.TextField()
     date_embauche = models.TextField()
 
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
+
 
 class INCIDENT(models.Model):
     id_incident = models.IntegerField(primary_key=True)
     description_incident = models.TextField()
     statut_incident = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
 
 
 class MISSION(models.Model):
@@ -42,6 +58,10 @@ class MISSION(models.Model):
     description_mission = models.TextField()
     etat_mission = models.TextField()
 
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
+
 
 class CONTIENT(models.Model):
     nom_foret = models.ForeignKey(
@@ -50,14 +70,22 @@ class CONTIENT(models.Model):
         ORGANISM, on_delete=models.CASCADE)
     nombre_organisme = models.IntegerField()
 
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
+
 
 class POSSEDE(models.Model):
     id_mission = models.ForeignKey(
-        MISSION, on_delete=models.CASCADE, primary_key=True)
+        MISSION, on_delete=models.CASCADE)
     id_garde = models.ForeignKey(
-        GARDE, on_delete=models.CASCADE)
+        GARDE, on_delete=models.CASCADE, primary_key=True)
     date_mission = models.TextField()
     etat_mission = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
 
 
 class TOUCHE(models.Model):
@@ -66,3 +94,7 @@ class TOUCHE(models.Model):
     id_incident = models.ForeignKey(
         INCIDENT, on_delete=models.CASCADE, primary_key=True)
     date_incident = models.TextField()
+
+    class Meta:
+        managed = False
+        db_table = 'forestDB'
