4227f7cf6124944e4ace7f4568b256e786516ff2
Maelle@83 <maelle.monier@yahoo.com>
2023-10-24 18:12:33 +0200

    synchronizing the branch

--- forestmanagementapp2/views.py
+++ forestmanagementapp2/views.py
@@ -1,8 +1,3 @@
 from django.shortcuts import render
 
-
-def home(request):
-    if request.method == 'POST':
-        choix = request.POST.get('choix', '')  # Récupérer la valeur sélectionnée dans le menu déroulant
-        # Effectuez ici le traitement en fonction du choix
-    return render(request, 'home.html')
+# Create your views here.
