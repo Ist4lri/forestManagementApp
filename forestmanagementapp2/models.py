7628349c159be4ba252321de49e2748c85feff08
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-11-14 09:54:28 +0100

    Update to be at same point

--- forestmanagementapp2/models.py
+++ forestmanagementapp2/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 class Contient(models.Model):
@@ -114,3 +115,19 @@ class Touche(models.Model):
     class Meta:
         managed = False
         db_table = 'TOUCHE'
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+    first_name = models.CharField(max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
