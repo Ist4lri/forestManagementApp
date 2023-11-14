e1dca76ce22c13d6dd08fb4d4034a976b8b20930
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-11-14 11:37:28 +0100

    saving

--- forestmanagementapp2/views.py
+++ forestmanagementapp2/views.py
@@ -64,7 +64,6 @@ def connexion(request):
         return render(request, 'login.html', {'image_path': image_path})
 
 
-
 def randomImage():
     return [fichier for fichier in os.listdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/forest_pic')) if fichier.lower().endswith(('.jpeg'))]
 
