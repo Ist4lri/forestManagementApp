4a2c47707e3fac5e65ab52dc08e7581fb31a6cfa
Ist4lri <pierreloic.gouttebel@gmail.com>
2023-11-16 15:56:01 +0100

    Begining to make something

--- forestmanagementapp2/models.py
+++ forestmanagementapp2/models.py
@@ -38,7 +38,7 @@ class Foret(models.Model):
         db_table = 'FORET'
 
 
-class Garde(AbstractUser):
+class Garde(models.Model): #AbstractUser
     id_garde = models.AutoField(primary_key=True)
     id_foret = models.ForeignKey(
         Foret, models.DO_NOTHING, db_column='id_foret')
